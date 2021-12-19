48a540221863a698c4dd07b327c1a8acce2ded8f
Amaanuddin <amaanuddinparvez@gmail.com>
2021-12-19 20:41:59 +0530

    sails with react setup

---
+++ assets/src/components/Todo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todo = () => {
+  return <div>This is test todo component</div>;
+};
+
+export default Todo;
---
+++ assets/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Todo from "./components/Todo";
+const App = () => {
+  return <Todo />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
